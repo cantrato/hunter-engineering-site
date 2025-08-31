1220caa6255d9f52d5c2a97a4f5b413deaf7b9ca
B <b@wmkbsl.com>
2025-08-30 17:47:31 -0700

    Remove copyright and social media links from footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,16 +2,10 @@ const Footer = () => {
   return (
     <footer className="bg-primary text-secondary p-4 mt-8">
       <div className="container mx-auto text-center">
-        <p>&copy; 2023 Hunter Engineering. All rights reserved.</p>
         <div className="mt-2">
           <a href="mailto:info@hunterengineers.com" className="text-link hover:underline">info@hunterengineers.com</a>
           <span className="mx-2">|</span>
           <span>Phone: (123) 456-7890</span>
-        </div>
-        <div className="mt-2">
-          <a href="#" className="text-link hover:underline mx-2">Facebook</a>
-          <a href="#" className="text-link hover:underline mx-2">LinkedIn</a>
-          <a href="#" className="text-link hover:underline mx-2">Twitter</a>
         </div>
       </div>
     </footer>
